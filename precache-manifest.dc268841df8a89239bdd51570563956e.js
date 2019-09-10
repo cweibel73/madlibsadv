ebb5e9b81c7aca67f896ea7fbc212b4e53b4f5f5
cweibel73 <carleweibel@gmail.com>
2019-09-09 19:01:00 -0700

    Updates

---
+++ precache-manifest.dc268841df8a89239bdd51570563956e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f116c85d9f1fc84dc6b3ef6f0c8c0e",
+    "url": "/madlibsadv/index.html"
+  },
+  {
+    "revision": "788f8f1a1a92fe9913dc",
+    "url": "/madlibsadv/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2b645e5c0c6c9a784c99",
+    "url": "/madlibsadv/static/js/2.27328f66.chunk.js"
+  },
+  {
+    "revision": "788f8f1a1a92fe9913dc",
+    "url": "/madlibsadv/static/js/main.5417006e.chunk.js"
+  },
+  {
+    "revision": "c76dd01b9ea0cb1a93b1",
+    "url": "/madlibsadv/static/js/runtime~main.930bd454.js"
+  }
+]);
